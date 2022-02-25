3891da794263f62e5f5e9cb2183c5596fa30397b
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2022-02-24 19:56:24 -0700

    Update stringly.py

--- stringly.py
+++ stringly.py
@@ -1,42 +1,82 @@
 def lower_left_triangle(s):
-    i = 0
-    while i< len(s):
-        i = i + 1
-        print(s[0:i])
+    '''
+    this function prints a triangle using the characters from the string argument "s"
+    Args:
+        s - is a string variable
+    return:
+        none
+    '''
+    index= 0
+    while index< len(s):
+        index= index+ 1
+        print(s[0:index])
 def lower_right_triangle(s):
-    i = len(s)-1
+    '''
+    this function prints a right hand triangle using the characters from the string argument "s"
+    Args:
+        s - is a string argument
+    return:
+        none
+    '''
+    index= len(s)-1
     e = 0
     o = len(s)
-    while i>=0:
+    while index>=0:
         e = e - 1
-        print(" "*(len(s)+e),end ="")
-        print(s[i:o])
-        i = i - 1
+        print(" "*(len(s)+e), end="")
+        print(s[index:o])
+        index = index - 1
 def wordly(word,guess ):
-    i = 0
+    '''
+    This function checks to see if the guess argument is the same as the word argument. It returns a string depending on
+    the amount of characters match in the two strings.
+    Args:
+        word - is a string argument that is the right answers
+        guess - is a string argument that is compaird to see what characters in the string match the ones in word
+
+    return:
+        correct - is a string built from the correcct characters in the guess argument
+    '''
+    index= 0
     correct=""
-    while i < len(guess):
-        if word[i] == guess[i]:
-            correct = correct+word[i]
-        elif word[i] in guess:
+    while index< len(guess):
+        if word[index] == guess[index]:
+            correct = correct+word[index]
+        elif word[index] in guess:
             correct = correct + "+"
         else:
-            correct = correct +" "
-        i = i+1
+            correct = correct + " "
+        index= index+1
     return correct
 def first_repetition(s):
-    i = 0
-    while i < len(s):
-        if s[i] == s[i+1]:
-            return i
-        i = i+1
+    '''
+    this functions check to see when the first set of rpepeating sharacters in the string argument "s" and returns a
+    variable depending on the index of the first character in the set
+    Args:
+        s - is a string variable that is checked
+    return:
+        an int depending on the index of the string characters
+    '''
+    index= 0
+    while index< len(s):
+        if s[index] == s[index+1]:
+            return index
+        ndex= index+1
     return -1
 def last_repetition(s):
-    i = len(s)-1
-    while i > 0:
-        if s[i] == s[i-1]:
-            return i-1
-        i = i -1
+    '''
+    this functions check to see when the last set of rpepeating sharacters in the string argument "s" and returns a
+    variable depending on the index of the last character in the set
+    Args:
+        s - is a string variable that is checked
+    return:
+        an int depending on the index of the string characters
+    '''
+    index= len(s)-1
+    while index> 0:
+        if s[index] == s[index-1]:
+            return index-1
+        index= index-1
     return -1
 def begins_with(s,prefix):
     if prefix == s[0:len(prefix)]:
@@ -44,8 +84,8 @@ def begins_with(s,prefix):
     else:
         return False
 def ends_with(s,suffix):
-    i = len(s)
-    if suffix == s[i-len(suffix):i]:
+    index= len(s)
+    if suffix == s[index-len(suffix):index]:
         return True
     else:
         return False
